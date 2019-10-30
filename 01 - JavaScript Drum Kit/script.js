2078571fcc7d2df3fd0a832388d50f8056c466b8
Karol Manijak <karol.manijak@grandparade.co.uk>
2019-10-30 22:52:01 +0100

    01 - initial solution

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,31 @@
+const transformToArray = collection => Array.from(collection);
+
+const getKeyElements = () => document.getElementsByClassName('key');
+const getAudioElements = () => document.getElementsByTagName('audio');
+const getKeyCode = el => el.dataset.key;
+
+const groupByKeyCodes = (keys, audios) => keys.reduce((acc, key) => {
+    const keyCode = getKeyCode(key);
+    const audio = audios.find(audio => getKeyCode(audio) == keyCode);
+
+    acc[keyCode] = { key, audio };
+    return acc;
+}, {})
+
+const keydownCallback = (keyCode) => {
+    const { key, audio } = elementsByKeyCode[keyCode] || {};
+
+    if (key && audio) {
+        key.classList.add('playing');
+        setTimeout(() => key.classList.remove('playing'), 70);
+
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
+const keys = transformToArray(getKeyElements());
+const audios = transformToArray(getAudioElements());
+const elementsByKeyCode = groupByKeyCodes(keys, audios);
+
+document.addEventListener('keydown', e => keydownCallback(e.keyCode));
