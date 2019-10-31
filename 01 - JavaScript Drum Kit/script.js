5208ab9bcd640248ee8a45e83cf13734dcd2dd57
Karol Manijak <karol.manijak@grandparade.co.uk>
2019-10-31 18:58:38 +0100

    01 - improvements after watching a video

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,8 +1,12 @@
 const transformToArray = collection => Array.from(collection);
-
 const getKeyElements = () => document.getElementsByClassName('key');
 const getAudioElements = () => document.getElementsByTagName('audio');
 const getKeyCode = el => el.dataset.key;
+function removeClass (e) {
+    if (e.propertyName === 'transform') {
+        this.classList.remove('playing');
+    }
+}
 
 const groupByKeyCodes = (keys, audios) => keys.reduce((acc, key) => {
     const keyCode = getKeyCode(key);
@@ -12,13 +16,11 @@ const groupByKeyCodes = (keys, audios) => keys.reduce((acc, key) => {
     return acc;
 }, {})
 
-const keydownCallback = (keyCode) => {
+const keydownCallback = ({ keyCode }) => {
     const { key, audio } = elementsByKeyCode[keyCode] || {};
 
     if (key && audio) {
         key.classList.add('playing');
-        setTimeout(() => key.classList.remove('playing'), 70);
-
         audio.currentTime = 0;
         audio.play();
     }
@@ -27,5 +29,6 @@ const keydownCallback = (keyCode) => {
 const keys = transformToArray(getKeyElements());
 const audios = transformToArray(getAudioElements());
 const elementsByKeyCode = groupByKeyCodes(keys, audios);
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
 
-document.addEventListener('keydown', e => keydownCallback(e.keyCode));
+document.addEventListener('keydown', keydownCallback);
